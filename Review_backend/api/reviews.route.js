010d1fad2da42374bc59d1291d45949d9068f7be
Nguyen Chi Hoang <chihoang.nguyen2709@gmail.com>
2024-05-29 17:37:53 +0700

    Second commit

--- Review_backend/api/reviews.route.js
+++ Review_backend/api/reviews.route.js
@@ -1,7 +1,10 @@
 import express from "express"
+import ReviewCtrl from "./reviews.controller.js"
 
 const router = express.Router()
 
-router.route("/").get((req, res) => res.send("hello world"))
+router.route("/movie/:id").get(ReviewCtrl.apiGetReviews)
+router.route("/new").post(ReviewCtrl.apiPostReview)
+router.route("/:id").get(ReviewCtrl.apiGetReview).put(ReviewCtrl.apiUpdateReview).delete(ReviewCtrl.apiDeleteReview)
 
 export default router
--- Review_backend/index.js
+++ Review_backend/index.js
@@ -1,11 +1,11 @@
 import app from "./server.js"
 import mongodb from "mongodb"
-// import ReviewsDAO from "./dao/ReviewsDAO.js"
+import ReviewsDAO from "../dao/reviewsDAO.js"
 
 const MongoClient = mongodb.MongoClient
-const mongo_username = process.env['MONGO_USERNAME']
-const mongo_password = process.env['MONGO_PASSWORD']
-const uri = `mongodb+srv://chihoangnguyen2709:<password>@cluster0.u0ogzcp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// const mongo_username = process.env['MONGO_USERNAME']
+// const mongo_password = process.env['MONGO_PASSWORD']
+const uri = "mongodb+srv://chihoangnguyen2709:hoang2003@cluster0.u0ogzcp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const port = 8000
 
 MongoClient.connect(
@@ -21,6 +21,7 @@ MongoClient.connect(
     process.exit(1)
 })
 .then(async client => {
+    await ReviewsDAO.injectDB(client)
     app.listen(port, () => {
         console.log(`listen on port ${port}`)
     })
